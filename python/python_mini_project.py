770f174398754be10dc6793e57955554df8b5981
Eddie Gurnee <pegurnee@gmail.com>
2015-01-13 19:16:53 -0500

    finished some more doc stuff

--- python/python_mini_project.py
+++ python/python_mini_project.py
@@ -1,6 +1,11 @@
 def at_len(the_list):
-    """Takes a list and returns the number
-    of atomic elements of the list"""
+    """
+    Takes a list and returns the number
+    of atomic elements (basic unit of data)
+    of the list
+    :param the_list: a list of elements
+    :return: the number of atomic elements in the list 
+    """
     count = 0
     for each_elem in the_list:
         if isinstance(each_elem, list):
