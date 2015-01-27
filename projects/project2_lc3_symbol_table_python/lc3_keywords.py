0731173aaf276c6b1be71a344076928860b97ec1
Eddie Gurnee <pegurnee@gmail.com>
2015-01-27 12:41:05 -0500

    added pseudo-ops and directives

--- projects/project2_lc3_symbol_table_python/lc3_keywords.py
+++ projects/project2_lc3_symbol_table_python/lc3_keywords.py
@@ -1,26 +1,16 @@
 opcodes = [
-  'add',
-  'and',
-  'not',
-  'ld',
-  'ldi',
-  'ldr',
-  'st',
-  'sti',
-  'str',
-  'lea',
-  'br',
-  'brn',
-  'brz',
-  'brp',
-  'brnz',
-  'brnp',
-  'brzp',
-  'brnzp',
-  'jsr',
-  'jsrr',
-  'jmp',
-  'rti',
-  'trap'
-
+  'add', 'and', 'not', 'ld', 'ldi', 'ldr',
+  'st', 'sti', 'str', 'lea', 'br', 'brn',
+  'brz', 'brp', 'brnz', 'brnp', 'brzp', 'brnzp',
+  'jsr', 'jsrr', 'jmp', 'rti', 'trap'
+]
+directives = [
+  '.orig', '.fill',
+  '.blkw', '.stringz',
+  '.end'
+]
+pseudoops = [
+  'getc', 'out',
+  'puts', 'in',
+  'putsp', 'halt'
 ]
