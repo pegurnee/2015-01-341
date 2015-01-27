749dec8eb74b805e2b28424e7fe035c801c34b0e
Eddie Gurnee <pegurnee@gmail.com>
2015-01-27 09:44:59 -0500

    lc3_keywords included

---
+++ projects/project2_lc3_symbol_table_python/lc3_keywords.py
@@ -0,0 +1,26 @@
+opcodes = [
+  'add',
+  'and',
+  'not',
+  'ld',
+  'ldi',
+  'ldr',
+  'st',
+  'sti',
+  'str',
+  'lea',
+  'br',
+  'brn',
+  'brz',
+  'brp',
+  'brnz',
+  'brnp',
+  'brzp',
+  'brnzp',
+  'jsr',
+  'jsrr',
+  'jmp',
+  'rti',
+  'trap'
+
+]
