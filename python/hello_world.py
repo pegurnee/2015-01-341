90eef3cb40683c2737d4193ac4e2a2bb4620a40f
Eddie Gurnee <pegurnee@gmail.com>
2015-02-04 19:28:35 -0500

    updated hello world to allow for no command line args

--- python/hello_world.py
+++ python/hello_world.py
@@ -2,7 +2,10 @@ import sys
 
 # hello world
 def main():
-    print ('Hello world', sys.argv[1])
+    if len(sys.argv) == 1:
+        print ('Hello World')
+    else:
+        print ('Hello world', sys.argv[1])
 
 if __name__ == '__main__':
     main()
