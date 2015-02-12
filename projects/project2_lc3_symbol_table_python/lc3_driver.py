3434139eee992d7701945b433badce2fc4dde508
Eddie Gurnee <pegurnee@gmail.com>
2015-02-11 20:03:50 -0500

    fixed cmd line args

--- projects/project2_lc3_symbol_table_python/lc3_driver.py
+++ projects/project2_lc3_symbol_table_python/lc3_driver.py
@@ -4,7 +4,7 @@ import lc3_asm_reader
 
 def main():
   if len(sys.argv) == 2:
-    filepath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + sys.argv[1]
+    filepath = sys.argv[1]
   else:
     filepath = input('Please enter the pathname of file to create: ')
     if not filepath:
