4f4083ce385268980c79191367189a55eda2e608
Eddie Gurnee <pegurnee@gmail.com>
2015-02-12 22:18:04 -0500

    tester now creates dictionary from expected and actual

--- projects/project2_lc3_symbol_table_python/lc3_table_tester.py
+++ projects/project2_lc3_symbol_table_python/lc3_table_tester.py
@@ -1,5 +1,36 @@
-def main():
-  pass
+import os
+import lc3_asm_reader
 
-if __name__ == '__main':
+def main():
+  filepath = input('Please enter the pathname of file to check: ')
+  if not filepath:
+    filepath = 'p1.asm'
+  
+  if not os.path.isfile(filepath):
+    print('file path is not a file.')
+    return
+  
+  actual = {}
+  expected = {}
+  
+  table = lc3_asm_reader.get_symbol_table(filepath)
+  display = table.__str__()
+  display = display.split('\n')
+  
+  for line in display:
+    if line[0] == '|':
+      the_line = line.split()
+      actual[the_line[1]] = the_line[3][2:]
+      print(line)
+  print(actual)
+  
+  with open(filepath[:-4]+'.sym') as file:
+    for line in file:
+      if line[2] == '\t':
+        the_line = line.split()
+        expected[the_line[1]] = the_line[2]
+        print(line.strip())
+  print(expected)
+  
+if __name__ == '__main__':
   main()
