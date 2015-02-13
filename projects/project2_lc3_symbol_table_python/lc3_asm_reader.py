1f7b54370f4a01f868827b53ab8cd2fe2bdf9af4
Eddie Gurnee <pegurnee@gmail.com>
2015-02-12 21:00:53 -0500

    added comments to lc3_asm_reader

--- projects/project2_lc3_symbol_table_python/lc3_asm_reader.py
+++ projects/project2_lc3_symbol_table_python/lc3_asm_reader.py
@@ -1,8 +1,15 @@
-def get_symbol_table(filename, absolute_flag='rel'):
+def get_symbol_table(filename, absolute_flag='r'):
+  '''
+  Returns a SymbolTable object from a given file.
+  :param filename: the name of the file to generate a
+  symbol table
+  :param absolute_flag: if the flag is set to absolute then
+  the filename string is absolute
+  '''
   from lc3_symbol_table import SymbolTable
   symbol_table = SymbolTable(filename)
 
-  if absolute_flag != 'abs':
+  if absolute_flag != 'a':
     import os
     currentpath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
     filename = currentpath + filename
@@ -19,6 +26,12 @@ def get_symbol_table(filename, absolute_flag='rel'):
   return symbol_table
 
 def is_lc3_instruction(token):
+  '''
+  Returns true if a given token is an lc3 opcode or
+  pseudo_op.
+  :param token: a string to test against the list of
+  opcodes and pseudo_ops
+  '''
   from lc3_keywords import opcodes
   for opcode in opcodes:
     if opcode == token.lower():
@@ -30,6 +43,16 @@ def is_lc3_instruction(token):
   return False
 
 def directive_analysis(line_segment, label='.none'):
+  '''
+  Checks what lc3 directive was used, returns the label
+  of the line that directive sits on, and the number of
+  lines that directive will take up. If no label exists,
+  '.none' is returned.
+  :param line_segment: the part of the line of code that
+  starts with an assembler directive
+  :param label: the label of the line that a given directive
+  is on, if it exists
+  '''
   #print(line_segment)
   if line_segment[:4].lower() == '.end':
     return '.end', 1
@@ -46,6 +69,12 @@ def directive_analysis(line_segment, label='.none'):
     return label, len(rest_of_line[1:rest_of_line.index('"', 1)]) + 1
   
 def decode_line(line):
+  '''
+  Takes in a line of code and returns the relevant symbol 
+  table information: label (if applicable) and number of lines 
+  the label takes up.
+  :param line: the line of code to interpret
+  '''
   line = line.strip();
   if line[0] == ';':
     return '.none', 0
