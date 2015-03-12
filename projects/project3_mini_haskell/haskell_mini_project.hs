78d89032b7e32d20b552717969ab42220d6b3182
Eddie Gurnee <pegurnee@gmail.com>
2015-03-12 01:32:41 -0400

    mini project finished 4,5

--- projects/project3_mini_haskell/haskell_mini_project.hs
+++ projects/project3_mini_haskell/haskell_mini_project.hs
@@ -1,3 +1,7 @@
+-- 2.0 --
+someNames = ["fluffy", "killer", "pooky", "skippy", "spot", "baron"]
+someVerbs = ["loves", "kicks", "bites", "feeds"]
+
 -- 2.a --
 createSquaredList [] = [0]
 createSquaredList theList = [x^2 | x <- theList]
@@ -15,10 +19,19 @@ isConsonant c = not $ isVowel c
 
 -- 2.d --
 --generateTwoSixSidedDiceThatValueSeven = [[dieOne, dieTwo] | dieOne <- [1..6], dieTwo <- [1..6], dieOne + dieTwo == 7]
-generateTwoSixSidedDiceThatValue num = [[dieOne, dieTwo] | dieOne <- [1..6], dieTwo <- [1..6], dieOne + dieTwo == num]
+generateTwoSixSidedDiceThatValueSeven = generateTwoDiceOfThatValue 6 7
+generateTwoSixSidedDiceThatValue num = generateTwoDiceOfThatValue 6 num
+--[[dieOne, dieTwo] | dieOne <- [1..6], dieTwo <- [1..6], dieOne + dieTwo == num]
+generateTwoDiceOfThatValue sides num = [[dieOne, dieTwo] | dieOne <- [1..sides], dieTwo <- [1..sides], dieOne + dieTwo == num]
+--generateDiceOfThatValue numDice sides targetNum = [[dieOne, dieTwo] | dieOne <- [1..sides], dieTwo <- [1..sides], dieOne + dieTwo == targetNum]
+--getDieValue side = die | die <- [1..sides]
+
+
 --generateDiceOptions 2 6 7
 --generateDiceOptions numDice numSide targetNumber = []
 
 -- 2.e --
+makingThreeSimpleSentences namesList verbsList = take 3 [(subject, verb, object) | subject <- namesList, verb <- verbsList, object <- namesList, subject /= object]
+testSentences = makingThreeSimpleSentences someNames someVerbs
 
 
