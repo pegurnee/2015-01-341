bbbd5f75dbda96755fb9b2b9fc05417f80916f28
Eddie Gurnee <pegurnee@gmail.com>
2015-03-12 23:20:30 -0400

    haskellize the first mini project

--- projects/project3_mini_haskell/haskell_mini_project.hs
+++ projects/project3_mini_haskell/haskell_mini_project.hs
@@ -5,30 +5,34 @@ createSquaredList [] = []
 createSquaredList theList = [x^2 | x <- theList]
 
 -- 2.b --
-createSquareRootList :: (Integral a) => [a] -> [a] 
-createSquareRootList theList = [truncate (sqrt (fromIntegral n)) | n <- theList, isSquare n]
+createSquareRootList :: (Integral a) => [a] -> [a]
+createSquareRootList theList = [truncate $ sqrt
+      $ fromIntegral n | n <- theList, isSquare n]
 isSquare :: (Integral a) => a -> Bool
-isSquare num = (floor (sqrt (fromIntegral num))) ^ 2 == num
+isSquare num = num == (^2) (floor $ sqrt $ fromIntegral num)
 
 -- 2.c --
 getVowelFreeString :: String -> String
-getVowelFreeString theString = [c | c <- theString, not $ isVowel c]
+getVowelFreeString theString = [c | c <- theString,
+      not $ isVowel c]
 isVowel :: Char -> Bool
-isVowel c = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+isVowel c = elem c ['a', 'e', 'i', 'o', 'u']
 isConsonant :: Char -> Bool
 isConsonant c = not $ isVowel c
 
 -- 2.d --
 generateTwoSixSidedDiceThatValueSeven :: [[Int]]
-generateTwoSixSidedDiceThatValueSeven = [[dieOne, dieTwo] 
-      | dieOne <- [1..6], 
-        dieTwo <- [1..6], 
+generateTwoSixSidedDiceThatValueSeven = [[dieOne, dieTwo]
+      | dieOne <- [1..6],
+        dieTwo <- [1..6],
         dieOne + dieTwo == 7]
 
 -- 2.e --
-makingThreeSimpleSentences :: [String] -> [String] -> [(String, String, String)]
-makingThreeSimpleSentences namesList verbsList = take 3 [(subject, verb, object) 
-      | subject <- namesList, 
-        verb <- verbsList, 
-        object <- namesList, 
+makingThreeSimpleSentences :: [String] -> [String] ->
+      [(String, String, String)]
+makingThreeSimpleSentences namesList verbsList =
+      take 3 [(subject, verb, object)
+      | subject <- namesList,
+        verb <- verbsList,
+        object <- namesList,
         subject /= object]
