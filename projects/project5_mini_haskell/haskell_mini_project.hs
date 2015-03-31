53dae2e7cb2242da7cffffb7db69ba9ab1560ece
Eddie Gurnee <pegurnee@gmail.com>
2015-03-30 21:59:45 -0400

    clean up scatter

--- projects/project5_mini_haskell/haskell_mini_project.hs
+++ projects/project5_mini_haskell/haskell_mini_project.hs
@@ -16,7 +16,7 @@ reduce :: (Num a) => (a -> a -> a) -> a -> [a] -> a
 reduce func num xs = foldl1 (func) $ num : xs
 
 -- 3 --
-scatter :: (Integral a) => a -> [t] -> [(a, [t])]
+scatter :: (Integral a) => a -> [b] -> [(a, [b])]
 scatter numberOfCores sharableData =
       combine possibleCores $ distributedData numberOfCores sharableData
   where
@@ -24,16 +24,15 @@ scatter numberOfCores sharableData =
     distributedData numberOfCores = zip (cycle possibleCores)
 
 combine [] _ = []
-combine (coreNum:remainingCores) [] =
-      (coreNum, []) : combine remainingCores []
+combine (coreNum:remainingCores) [] = (coreNum, []) : combine remainingCores []
 combine (coreNum:remainingCores) all@(x:xs) =
       grabAllData coreNum all : combine remainingCores (removeExtraData coreNum xs)
-  where
-    grabAllData key theData = (key, [snd tuple | tuple <- theData, fst tuple == key])
-    removeExtraData key = filter (\x -> fst x /= key)
+
+grabAllData key theData = (key, [snd tuple | tuple <- theData, fst tuple == key])
+removeExtraData key = filter (\x -> fst x /= key)
 
 -- 4 --
---gather ::
+gather :: (Num a) => [(a, [b])] -> (a, [b])
 gather allDistributedData = (numCores, recombineData allDistributedData)
   where
     numCores = fst (last allDistributedData) + 1
