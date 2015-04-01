af9b4cbebd8d7d0005c7ca4c5ef6b8aaed5b5ed0
Eddie Gurnee <pegurnee@gmail.com>
2015-03-31 23:55:43 -0400

    erlang, probably not entirely useless

---
+++ erlang/useless.erl
@@ -0,0 +1,14 @@
+-module(useless).
+-export([add/2, hello/0, greet_and_add_two/1]).
+-author("Eddie | egurnee@emich.edu").
+
+% comment
+add(A,B) ->
+  A + B.
+
+%% Stylish comment
+hello() ->
+  io:format("Hello, world!~n").
+
+greet_and_add_two(X) ->
+  add(X,2).
