682f3d08a79a99c90030a56a1c6b5eb78644090b
Eddie Gurnee <pegurnee@gmail.com>
2015-04-01 00:40:44 -0400

    erlang, now functions!

--- erlang/functions.erl
+++ erlang/functions.erl
@@ -2,14 +2,20 @@
 
 
 -module(functions).
--author("eddie").
+-author(eddie).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([same/2, second/1, head/1, valid_date/1]).
-
-
+-export([
+         same/2, 
+         second/1, 
+         head/1, 
+         valid_date/1, 
+         old_enough/1, 
+         right_age/1, 
+         wrong_age/1
+        ]).
 
 %% ====================================================================
 %% Internal functions
@@ -26,3 +32,12 @@ valid_date({Date = {Y,M,D}, Time = {H,Min,S}}) ->
     io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time,H,Min,S]);
 valid_date(_) ->
     io:format("Wrong data my friend!!!~n").
+
+old_enough(X) when X >= 16 -> true;
+old_enough(_) -> false.
+
+right_age(X) when X >= 16, X =< 104 -> true;
+right_age(_) -> false.
+
+wrong_age(X) when X < 16; X > 104 -> true;
+wrong_age(_) -> false.
