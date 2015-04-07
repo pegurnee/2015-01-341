774779f47a0a7b99b26c88fa6a370a695c0d6de9
Eddie Gurnee <pegurnee@gmail.com>
2015-04-07 14:52:01 -0400

    what the if finished

--- erlang/what_the_if.erl
+++ erlang/what_the_if.erl
@@ -20,6 +20,45 @@ heh_fine() ->
          fails
   end.
 
+oh_god(N) ->
+  if N =:= 2 -> might_succeed;
+    true -> always_succeed
+  end.
+
+help_me(Animal) ->
+  Talk = if Animal == cat -> "meow";
+            Animal == beef -> "mooo";
+            Animal == dog -> "bark";
+            Animal == tree -> "bark";
+            true -> "asdfaksdfj"
+         end,
+  {Animal, "says " ++ Talk ++ "!"}.
+
+insert(X, []) ->
+    [X];
+insert(X, Set) ->
+    case lists:member(X, Set) of
+        true -> Set;
+        false -> [X|Set]
+    end.
+
+beach(Temperature) ->
+    case Temperature of
+        {celsius, N} when N >= 20, N =< 45 ->
+            'favorable';
+        {kelvin, N} when N >= 293, N =< 318 ->
+            'scientifically favorable';
+        {fahrenheit, N} when N >= 68, N =< 113 ->
+            'favorable in the US';
+        _ ->
+            'avoid beach'
+    end.
+
+beachf({celsius, N}) when N > 20, N =< 45 ->
+  'favorable';
+beachf(_) ->
+  'avoid beach'.
+
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
