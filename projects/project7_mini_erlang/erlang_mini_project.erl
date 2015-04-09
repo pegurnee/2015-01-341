681a2190eefdb371403bfa86536580b658147962
Eddie Gurnee <pegurnee@gmail.com>
2015-04-09 15:02:29 -0400

    ackermann in erlang

--- projects/project7_mini_erlang/erlang_mini_project.erl
+++ projects/project7_mini_erlang/erlang_mini_project.erl
@@ -1,7 +1,6 @@
 %% @author eddie
 %% @doc @todo Add description to erlang_mini_project.
 
-
 -module(erlang_mini_project).
 -author(eddie).
 
@@ -10,17 +9,20 @@
 %% ====================================================================
 -export([count/2, substring/2, get_last/1, zip_pairs/2, permute/1, ackermann/2]).
 
-count(X,Y).
+count(X,Y) -> false.
 
-substring(X,Y).
+substring(X,Y) -> false.
 
-get_last(X).
+get_last(X) -> false.
 
-zip_pairs(X,Y).
+zip_pairs(X,Y) -> false.
 
-permute(X).
+permute(X) -> false.
 
-ackermann(M,N).
+ackermann(M, N) when M < 0; N < 0 -> fail_blog;
+ackermann(0, N) -> N + 1;
+ackermann(M, 0) -> ackermann(M - 1, 1);
+ackermann(M, N) -> ackermann(M - 1, ackermann(M, N - 1)).
 
 %% ====================================================================
 %% Internal functions
