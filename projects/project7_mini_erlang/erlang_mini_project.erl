4f3c1dd9b07e9749ae8e3fdf169bf6a817b1c344
Eddie Gurnee <pegurnee@gmail.com>
2015-04-09 14:30:56 -0400

    added erlang mini project

---
+++ projects/project7_mini_erlang/erlang_mini_project.erl
@@ -0,0 +1,29 @@
+%% @author eddie
+%% @doc @todo Add description to erlang_mini_project.
+
+
+-module(erlang_mini_project).
+-author(eddie).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([count/2, substring/2, get_last/1, zip_pairs/2, permute/1, ackermann/2]).
+
+count(X,Y).
+
+substring(X,Y).
+
+get_last(X).
+
+zip_pairs(X,Y).
+
+permute(X).
+
+ackermann(M,N).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
