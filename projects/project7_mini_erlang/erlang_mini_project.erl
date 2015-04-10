62f70988e9085363a69533a429d0e9afc6f6a5f8
Eddie Gurnee <pegurnee@gmail.com>
2015-04-09 23:19:02 -0400

    erlang mini project formatted

--- projects/project7_mini_erlang/erlang_mini_project.erl
+++ projects/project7_mini_erlang/erlang_mini_project.erl
@@ -9,14 +9,34 @@
 %% ====================================================================
 -export([count/2, substring/2, get_last/1, zip/2, permute/1, ackermann/2]).
 
-count(X, []) -> 0;
-count(X, [Y | YS]) -> 
+count(_, []) -> 0;
+count(X, [Y|YS]) -> 
     case X =:= Y of
         true -> 1 + count(X, YS);
         false -> count(X, YS)
     end.
 
 substring(X,Y) -> subHelp(0, X, Y).
+
+get_last([]) -> [];
+get_last([X]) -> X;
+get_last([_, Y|Tail]) -> get_last([Y|Tail]).
+
+zip([], _) -> [];
+zip(_, []) -> [];
+zip([Head1|Tail1], [Head2|Tail2]) -> [{Head1, Head2}|zip(Tail1, Tail2)].
+
+permute([]) -> [[]];
+permute(List) -> [[Head|Tail] || Head <- List, Tail <- permute(List -- [Head])].
+
+ackermann(M, N) when M < 0; N < 0 -> fail_blog;
+ackermann(0, N) -> N + 1;
+ackermann(M, 0) -> ackermann(M - 1, 1);
+ackermann(M, N) -> ackermann(M - 1, ackermann(M, N - 1)).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
 
 subHelp(_, _, []) -> -1;
 subHelp(Index, X, Y) ->
@@ -27,29 +47,9 @@ subHelp(Index, X, Y) ->
 
 take([], _) -> [];
 take(_, 0) -> [];
-take([Head | Tail], N) -> [Head | take(Tail, N - 1)].
+take([Head|Tail], N) -> [Head|take(Tail, N - 1)].
 
 tail([]) -> [];
-tail([H | T]) -> T.
-
-get_last([]) -> [];
-get_last([X]) -> X;
-get_last([X, Y | Tail]) -> get_last([Y | Tail]).
-
-zip([], _) -> [];
-zip(_, []) -> [];
-zip([Head1 | Tail1], [Head2 | Tail2]) -> [{Head1, Head2} | zip(Tail1, Tail2)].
-
-permute([]) -> [[]];
-permute(List) -> [[Head | Tail] || Head <- List, Tail <- permute(List -- [Head])].
-
-ackermann(M, N) when M < 0; N < 0 -> fail_blog;
-ackermann(0, N) -> N + 1;
-ackermann(M, 0) -> ackermann(M - 1, 1);
-ackermann(M, N) -> ackermann(M - 1, ackermann(M, N - 1)).
-
-%% ====================================================================
-%% Internal functions
-%% ====================================================================
+tail([_|T]) -> T.
 
 
